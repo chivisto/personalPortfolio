de050e29073bb73b4b17335ba8d45ed8a1d0d67d
Isaac Covarrubias <chivisto@live.com>
2020-07-21 19:58:25 -0600

    Added js function

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 import films from './films.js';
 
-for (let i = 0; i < films.length; i++) {
-    console.log(films[i]);
+function allMovies() {
+    for (let i = 0; i < films.length; i++) {
+        console.log(films[i]);
+    }
 }
+allMovies();
